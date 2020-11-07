c8f2bcb3d837302bbacf4d12d203e0f8239f74de
samipourquoi <sami.guiderdoni@gmail.com>
2020-11-07 18:14:14 +0100

    Ehance exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-import { Command } from "./command";
-import { NumberType, QuotedType, Type, UnquotedStringType, WordType } from "./types";
-
-export {
-	Command,
-
-	// Types
-	Type,
-	WordType,
-	UnquotedStringType,
-	NumberType,
-	QuotedType
-}
+export * from "./command";
+export * from "./types";
